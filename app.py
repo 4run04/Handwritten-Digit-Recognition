ad6a333f617e9e2a6d03e6fc59805f4e56793a23
S Arun Prakash <110722446+4run04@users.noreply.github.com>
2025-01-26 19:50:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,10 @@ from streamlit_drawable_canvas import st_canvas
 st.title("Handwritten Digit Recognition (Pickle Version)")
 st.write("Draw a digit (0-9) in the box below and click 'Predict' to see the result.")
 
-# Debugging information
-st.write(f"Current working directory: {os.getcwd()}")
-st.write(f"Does the default model file exist? {'Yes' if os.path.exists('mnist_cnn_model.pkl') else 'No'}")
+# Debug
+#st.write(f"Current working directory: {os.getcwd()}")
+#st.write(f"Does the default model file exist? {'Yes' if os.path.exists('mnist_cnn_model.pkl') else 'No'}")
 
-# Load the model
 model = None
 try:
     with open("mnist_cnn_augmented_model.pkl", "rb") as file:
